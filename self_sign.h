7dcadff705dc16297107305199404bf3dba39ad7
Nathaniel <bennettnate5@outlook.com>
2020-04-30 09:36:47 -0600

    Removed all files not actively in dev or use

--- self_sign.h
+++
@@ -1,7 +0,0 @@
-#ifndef SELF_SIGN_H
-#define SELF_SIGN_H
-
-int generate_rsa_key(EVP_PKEY** key_out, int bits);
-X509* generate_self_signed_certificate(EVP_PKEY* key, int serial, int days);
-
-#endif
