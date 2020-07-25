91e53a517f78699468e446da29c7e76a320a4854
Nathaniel <nathaniel.bennett@tutanota.com>
2020-07-25 12:31:19 -0600

    Added session reuse disabled test

--- test_files/sessions_client.cpp
+++ test_files/sessions_client.cpp
@@ -19,13 +19,15 @@ extern "C" {
 #define TWO_FDS 2
 #define TEN_FDS 10
 
-const char* HOSTNAME  = "ebay.com"; /* Does sessions :thumbsup: */
+const char* HOSTNAME  = "localhost"; /* Does sessions */
 const char* PORT = "443";
 
 const char* ERR_SOURCE_GET_CONTEXT = "TLS_CONTEXT getsockopt()";
 const char* ERR_SOURCE_SET_HOSTNAME = "TLS_HOSTNAME setsockopt()";
 const char* ERR_SOURCE_CONNECT = "connect()";
 const char* ERR_SOURCE_SESS_RESUMED = "TLS_RESUMED_SESSION getsockopt()";
+const char* ERR_SOURCE_DISABLE_SESS = "TLS_SESSION_REUSE setsockopt()";
+
 }
 
 
@@ -44,16 +46,17 @@ public:
         if (ret != 0)
             goto err;
 
-        context_fd = socket(AF_INET, SOCK_STREAM, 0);
+        context_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TLS);
         if (context_fd < 0) {
             perror("socket() failed");
             goto err;
         }
 
         hostname_ret = setsockopt(context_fd, IPPROTO_TLS, 
-                    TLS_HOSTNAME, &HOSTNAME, strlen(HOSTNAME)+1);
+                TLS_HOSTNAME, HOSTNAME, strlen(HOSTNAME)+1);
+
         if (hostname_ret != 0) {
-            print_socket_error(tls_context, ERR_SOURCE_SET_HOSTNAME);
+            print_socket_error(context_fd, ERR_SOURCE_SET_HOSTNAME);
             goto err;
         }
 
@@ -68,6 +71,7 @@ public:
             print_socket_error(context_fd, ERR_SOURCE_GET_CONTEXT);
             goto err;
         }
+
 
         return;
     err:
@@ -171,10 +175,9 @@ TEST_F(ClientSessionTests, TwoTLSContextsNoReuse) {
         ASSERT_EQ(session_reused_ret, 0);
 
         ASSERT_EQ(session_reused, 0);
-    }
 
-    for (int i = 0; i < TWO_FDS; i++)
         close(fds[i]);
+    }
 }
 
 TEST_F(ClientSessionTests, TwoSocketSessionReuse) {
@@ -197,6 +200,15 @@ TEST_F(ClientSessionTests, TwoSocketSessionReuse) {
         
         EXPECT_EQ(context_errno, 0);
         ASSERT_EQ(context_ret, 0);
+
+        int hostname_ret = setsockopt(fds[i], IPPROTO_TLS, 
+                    TLS_HOSTNAME, HOSTNAME, strlen(HOSTNAME)+1);
+        int hostname_errno = errno;
+        if (hostname_ret != 0)
+            print_socket_error(tls_context, ERR_SOURCE_SET_HOSTNAME);
+        
+        EXPECT_EQ(hostname_errno, 0);
+        ASSERT_EQ(hostname_ret, 0);
 
         int connect_ret = connect(fds[i], address, addrlen);
         int connect_errno = errno;
@@ -221,10 +233,9 @@ TEST_F(ClientSessionTests, TwoSocketSessionReuse) {
             ASSERT_EQ(session_reused, 0);
         else
             ASSERT_EQ(session_reused, 1);
-    }
 
-    for (int i = 0; i < TWO_FDS; i++)
         close(fds[i]);
+    }
 }
 
 TEST_F(ClientSessionTests, TenSocketSessionReuse) {
@@ -234,7 +245,7 @@ TEST_F(ClientSessionTests, TenSocketSessionReuse) {
     socklen_t int_len = sizeof(int);
     int fds[TEN_FDS];
 
-    for (int i = 0; i < TWO_FDS; i++) {
+    for (int i = 0; i < TEN_FDS; i++) {
         fds[i] = socket(AF_INET, SOCK_STREAM, IPPROTO_TLS);
         if (fds[i] < 0)
             perror("socket() failed");
@@ -248,6 +259,16 @@ TEST_F(ClientSessionTests, TenSocketSessionReuse) {
         
         EXPECT_EQ(context_errno, 0);
         ASSERT_EQ(context_ret, 0);
+
+
+        int hostname_ret = setsockopt(fds[i], IPPROTO_TLS, 
+                    TLS_HOSTNAME, HOSTNAME, strlen(HOSTNAME)+1);
+        int hostname_errno = errno;
+        if (hostname_ret != 0)
+            print_socket_error(tls_context, ERR_SOURCE_SET_HOSTNAME);
+        
+        EXPECT_EQ(hostname_errno, 0);
+        ASSERT_EQ(hostname_ret, 0);
 
         int connect_ret = connect(fds[i], address, addrlen);
         int connect_errno = errno;
@@ -270,6 +291,17 @@ TEST_F(ClientSessionTests, TenSocketSessionReuse) {
 
         ASSERT_GE(session_reused, 0);
         ASSERT_LE(session_reused, 1);
+
+        /*
+        int send_ret = send(fds[i], "GET / HTTP/1.1\r\n\r\n", strlen("GET / HTTP/1.1\r\n\r\n"), 0);
+        */
+
+        /*
+        char buf[1000];
+        int recv_ret = recv(fds[i], buf, 1000, 0);
+        */
+
+        close(fds[i]);
         
         if (i == 0)
             ASSERT_EQ(session_reused, 0);
@@ -280,12 +312,74 @@ TEST_F(ClientSessionTests, TenSocketSessionReuse) {
     ASSERT_GT(num_sessions_reused, 0);
 
     printf("Total sessions reused: %i\n", num_sessions_reused);
-
-    for (int i = 0; i < TEN_FDS; i++)
-        close(fds[i]);
 }
 
 TEST_F(ClientSessionTests, TwoSocketDisabledSessionReuse) {
+
+    int session_reused;
+    socklen_t int_len = sizeof(int);
+    int fds[TWO_FDS];
+    int reuse_sessions = 0;
+
+    for (int i = 0; i < TWO_FDS; i++) {
+        fds[i] = socket(AF_INET, SOCK_STREAM, IPPROTO_TLS);
+        if (fds[i] < 0)
+            perror("socket() failed");
+
+        int context_ret = setsockopt(fds[i], IPPROTO_TLS, 
+                    TLS_CONTEXT, &tls_context, sizeof(tls_context));
+        int context_errno = errno;
+
+        int disable_session_ret = setsockopt(fds[i], IPPROTO_TLS,
+                    TLS_SESSION_REUSE, &reuse_sessions, sizeof(reuse_sessions));
+        int disable_session_errno = errno;
+
+        if (disable_session_ret != 0)
+            print_socket_error(fds[i], ERR_SOURCE_DISABLE_SESS);
+
+        EXPECT_EQ(disable_session_errno, 0);
+        ASSERT_EQ(disable_session_ret, 0);
+
+        if (context_ret != 0)
+            print_socket_error(fds[i], ERR_SOURCE_SET_HOSTNAME);
+        
+        EXPECT_EQ(context_errno, 0);
+        ASSERT_EQ(context_ret, 0);
+
+        int hostname_ret = setsockopt(fds[i], IPPROTO_TLS, 
+                    TLS_HOSTNAME, HOSTNAME, strlen(HOSTNAME)+1);
+        int hostname_errno = errno;
+        if (hostname_ret != 0)
+            print_socket_error(tls_context, ERR_SOURCE_SET_HOSTNAME);
+        
+        EXPECT_EQ(hostname_errno, 0);
+        ASSERT_EQ(hostname_ret, 0);
+
+        int connect_ret = connect(fds[i], address, addrlen);
+        int connect_errno = errno;
+
+        if (connect_ret != 0)
+            print_socket_error(fds[i], ERR_SOURCE_CONNECT);
+
+        EXPECT_EQ(connect_errno, 0);
+        ASSERT_EQ(connect_errno, 0);
+
+        int session_reused_ret = getsockopt(fds[i], IPPROTO_TLS,
+                    TLS_RESUMED_SESSION, &session_reused, &int_len);
+        int session_reused_errno = errno;
+
+        if (session_reused_ret != 0)
+            print_socket_error(fds[i], ERR_SOURCE_SESS_RESUMED);
+
+        EXPECT_EQ(session_reused_errno, 0);
+        ASSERT_EQ(session_reused_ret, 0);
+        
+        /* we want sessions not to be reused here */
+        ASSERT_EQ(session_reused, 0);
+    }
+
+    for (int i = 0; i < TWO_FDS; i++)
+        close(fds[i]);
 
 }
 
